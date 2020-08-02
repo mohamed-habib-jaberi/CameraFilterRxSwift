3e364d19c57ba30f17a69cb4646ba17e4d89fb51
mohamed  habib <mohamedhabibjaberi111@gmail.com>
2020-08-02 17:58:15 +0200

    Initial Commit

---
+++ CameraFilterRxSwift/PhotosCollectionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PhotosCollectionViewController.swift
+//  CameraFilterRxSwift
+//
+//  Created by mohamed  habib on 02/08/2020.
+//  Copyright © 2020 mohamed  habib. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PhotosCollectionViewController: UICollectionViewController {
+    
+    
+}
---
+++ CameraFilterRxSwift/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  CameraFilterRxSwift
+//
+//  Created by mohamed  habib on 02/08/2020.
+//  Copyright © 2020 mohamed  habib. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        
+    }
+
+
+}
+
