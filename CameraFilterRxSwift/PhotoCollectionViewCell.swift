38344adad1947ed77b5ea94e8b45b5e0bf8efc58
mohamed  habib <mohamedhabibjaberi111@gmail.com>
2020-08-02 20:26:07 +0200

    Displaying Photos from Photo Library

---
+++ CameraFilterRxSwift/PhotoCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoCollectionViewCell.swift
+//  CameraFilterRxSwift
+//
+//  Created by mohamed  habib on 02/08/2020.
+//  Copyright © 2020 mohamed  habib. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+}
